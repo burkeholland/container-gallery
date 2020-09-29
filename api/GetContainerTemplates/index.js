6c652e1276b41a60ac9a3308170118a5342fe150
Burke Holland <burkeholland@gmail.com>
2020-09-29 14:21:44 -0500

    Change app location

--- api/GetContainerTemplates/index.js
+++ api/GetContainerTemplates/index.js
@@ -1,22 +1,29 @@
 const axios = require("axios");
 
 module.exports = async function (context, req) {
-  const results = await axios.get(
-    `https://api.github.com/repos/${process.env.REPO}/contents/${process.env.FOLDER}`,
-    {
-      headers: {
-        Authorization: `token ${process.env.GITHUB_PAT}`,
-      },
-    }
-  );
+  try {
+    const results = await axios.get(
+      `https://api.github.com/repos/${process.env.REPO}/contents/${process.env.FOLDER}`,
+      {
+        headers: {
+          Authorization: `token ${process.env.GITHUB_PAT}`,
+        },
+      }
+    );
 
-  // this folder contains all of the container template folders
-  // return only the items that are folders
-  const folders = results.data.filter((item) => {
-    return item.type === "dir";
-  });
+    // this folder contains all of the container template folders
+    // return only the items that are folders
+    const folders = results.data.filter((item) => {
+      return item.type === "dir";
+    });
 
-  context.res = {
-    body: folders,
-  };
+    context.res = {
+      body: folders,
+    };
+  } catch (err) {
+    context.res = {
+      status: 500,
+      body: err,
+    };
+  }
 };
